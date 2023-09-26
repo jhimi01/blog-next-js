ad5ba1b7ce6a2c18c2f333b28282e73ebcfe9e37
Jhimi Talukder <talukderjhimi@gmail.com>
2023-09-26 19:20:41 +0600

    header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaCloudMoon } from 'react-icons/fa';
-import { BsFillCloudSunFill } from 'react-icons/bs';
+import { BsFacebook, BsFillCloudSunFill, BsYoutube } from 'react-icons/bs';
+import { AiOutlineTwitter } from 'react-icons/ai';
 
 export default function Header() {
 
@@ -21,19 +22,19 @@ export default function Header() {
   
     return (
      <div className="p-4 header-bg">
-      <div className="flex items-center justify-between w-11/12 mx-auto">
+      <div className="md:flex items-center justify-between custom-width">
       <div className="flex items-center gap-6">
-        <input type="text" className="rounded-full py-1 px-3 bg-transparent border" placeholder="search..."/>
-        <button onClick={toggleTheme} className="text-3xl">{isDarkMode ? <BsFillCloudSunFill /> : <FaCloudMoon />}</button>
+        <input type="text" className="rounded-full py-1 pl-3 bg-transparent border" placeholder="search..."/>
+        <button onClick={toggleTheme} className={`text-3xl ${!isDarkMode && 'text-[#333]'}`}>{isDarkMode ? <BsFillCloudSunFill /> : <FaCloudMoon />}</button>
       </div>
-      <div className="text-3xl font-semibold">
+      <div className={`text-3xl font-semibold ${!isDarkMode && 'text-[#333]'}`}>
         <a>Design</a>
       </div>
       <div>
-        <div className="flex gap-4 text-xl">
-          <a>Facebook</a>
-          <a>Twitter</a>
-          <a>Youtube</a>
+        <div className={`flex gap-6 text-3xl ${!isDarkMode && 'text-[#333]'}`}>
+          <a><BsFacebook /></a>
+          <a><AiOutlineTwitter /></a>
+          <a><BsYoutube /></a>
         </div>
       </div>
      </div>
