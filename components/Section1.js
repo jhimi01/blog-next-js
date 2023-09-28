d05be27fc31de8f04f82b85430b468f8ed196505
Jhimi Talukder <talukderjhimi@gmail.com>
2023-09-28 19:51:26 +0600

    banner

--- components/Section1.js
+++ components/Section1.js
@@ -6,33 +6,53 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 export default function Section1() {
+  const images = [
+    '/images/img (1).jpg',
+    '/images/img (2).jpg',
+    // '/images/img (3).jpg',
+    '/images/img (4).jpg',
+    '/images/img (5).jpg',
+    '/images/img (6).jpg',
+    '/images/img (7).jpg',
+  ]
+  
   return (
-    <div>
-      <h1 className='text-center text-3xl font-semibold my-5'>Trending</h1>
+    <div className='my-5'>
+      {/* <h1 className='text-center text-3xl font-semibold my-5'>Trending</h1> */}
 
       <Swiper
-      spaceBetween={20}
+      spaceBetween={30}
       slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide> {Slide()}</SwiperSlide>
-      <SwiperSlide> {Slide()}</SwiperSlide>
-      <SwiperSlide> {Slide()}</SwiperSlide>
-      <SwiperSlide> {Slide()}</SwiperSlide>
+      {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Slide imageSource={image} />
+          </SwiperSlide>
+        ))}
     </Swiper>
      
     </div>
   )
 }
 
-function Slide() {
+function Slide({imageSource}) {
   return (
-    <div className=''>
+    <div>
+    <div className='image'>
+      <Image src={imageSource} width={1000} height={900} alt='blog image' />
+    </div>
+  </div>
+   
+  )
+}
+
+{/* <div className=''>
 <div className='image'>
   <Image src={'/images/img (2).jpg'} width={1000} height={900} alt='blog image'></Image>
 </div>
-{/* <div className='info'>
+<div className='info'>
 <div className='cat'>
   <Link legacyBehavior href={'/'}><a className='text-[#74b9ff]'>Business, Travel</a></Link>
   <Link legacyBehavior href={'/'}><a className='text-[#b2bec3]'>-july-04-2023</a></Link>
@@ -42,7 +62,5 @@ function Slide() {
   <p className='text-[#959899]'>Societal Marketing is based on the principle of societal welfare. It emphasizes that an organization must make strategic marketing decisions keeping in mind the consumer wants, the organizational needs and most important the long term interests of the society.</p>
   <h5>author</h5>
 </div>
-</div> */}
-    </div>
-  )
-}
+</div>
+    </div> */}
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
    <div className='custom-width'>
    {/* <Helmet text='blog'></Helmet> */}
    <Head><title>Blog</title></Head>
-   {/* <h1 className='text-6xl py-5 font-semibold text-center border-b'>Blog</h1> */}
+   <h1 className='text-6xl py-5 font-semibold text-center border-b'>Blog</h1>
    <Section1></Section1>
    </div>
   )
