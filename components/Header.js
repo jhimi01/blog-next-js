a4fb351f2609ddbbc59ba4c256989285f82a58ef
Jhimi Talukder <talukderjhimi@gmail.com>
2023-09-30 19:42:20 +0600

    material ui

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import { IoLogoWhatsapp } from 'react-icons/io';
 // import Link from "next/link";
 import useMood from "@/hooks/useMood";
 import Link from 'next/link';
+import { TextField } from '@mui/material';
 
 export default function Header() {
 
@@ -15,6 +16,9 @@ export default function Header() {
       <div className="flex items-center justify-between custom-width gap-1 sm:gap-0">
       <div className="flex items-center gap-1 sm:gap-6">
         <input style={{"boxShadow": "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px"}} type="text" className="input-style" placeholder="search..."/>
+        {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+        {/* <TextField id="standard-basic" label="Standard" variant="standard" /> */}
         <button onClick={toggleTheme} className={`text-3xl ${!isDarkMode && 'text-[#333]'}`}>{isDarkMode ? <BsFillCloudSunFill /> : <FaCloudMoon />}</button>
       </div>
        {/* <div className={`text-3xl font-semibold uppercase ${!isDarkMode && 'text-[#333]'}`}>
