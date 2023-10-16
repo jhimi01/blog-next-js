1a98ece876afb64e1a0f244b61129f833cfdf149
Jhimi Talukder <talukderjhimi@gmail.com>
2023-10-16 18:33:03 +0600

    json data

--- components/Section2.js
+++ components/Section2.js
@@ -1,31 +1,31 @@
 
-// export default function Section2() {
-//   return (
-//     <div>
-//       <h1 className='text-center text-3xl font-semibold my-5'>Trending</h1>
-      
-//     </div>
-//   )
-// }
-
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
 export default function Section2() {
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-    </Box>
-  );
+    <div>
+      <h1 className='text-center text-3xl font-semibold my-5'>Trending</h1>
+      
+    </div>
+  )
 }
+
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+
+// export default function Section2() {
+//   return (
+//     <Box
+//       component="form"
+//       sx={{
+//         '& > :not(style)': { m: 1, width: '25ch' },
+//       }}
+//       noValidate
+//       autoComplete="off"
+//     >
+//       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+//       <TextField id="filled-basic" label="Filled" variant="filled" />
+//       <TextField id="standard-basic" label="Standard" variant="standard" />
+//     </Box>
+//   );
+// }
 
