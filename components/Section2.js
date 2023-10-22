8cda2c732de95290fdb10a705183b01b14c89273
Jhimi Talukder <talukderjhimi@gmail.com>
2023-10-22 18:37:20 +0600

    bg gradient

--- components/Section2.js
+++ components/Section2.js
@@ -3,7 +3,6 @@ export default function Section2() {
   return (
     <div>
       <h1 className='text-center text-3xl font-semibold my-5'>Trending</h1>
-      
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,16 @@ export default function Home() {
    {/* <Helmet text='blog'></Helmet> */}
    <Head><title>Blog</title></Head>
    <div className="border-b py-10 md:px-0 px-3 md:space-y-5">
+  
         <p className='uppercase font-thin w-2/3 ml-auto ' style={{ letterSpacing: '2px' }}>a <span className='border-b border-[#747373]'>personal</span> blog</p>
         <h1  data-aos="fade-right"  data-aos-easing="ease-in-out" 
         data-aos-duration="1200"
          className={`text-7xl md:text-[160px] font-semibold text-center`}>Ven<span style={style}>ture</span>r</h1>
         <p data-aos="fade-left"  data-aos-easing="ease-in-out"
-        data-aos-duration="1200" className='text-[#b1b1b1] text-4xl font-extralight md:w-2/3 ml-auto text-justify md:text-start md:pl-32' style={{ letterSpacing: '3px' }}>I am a Blogger & Photographer
+        data-aos-duration="1200" className=' text-4xl font-extralight md:w-2/3 ml-auto text-justify md:text-start md:pl-32' style={{ letterSpacing: '3px' }}>I am a Blogger & Photographer
 Based in the world.</p>
       </div>
+      <span className='bg-gradient-cusotom'></span>
    <Section1></Section1>
    <Section2></Section2>
    </div>
