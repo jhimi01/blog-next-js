747bf77eaa5ceecc4b5e078d856fffc1d9de250a
Jhimi Talukder <talukderjhimi@gmail.com>
2023-11-07 18:44:50 +0600

    button change

--- components/Footer.js
+++ components/Footer.js
@@ -57,7 +57,7 @@ export default function Footer() {
                 className="p-3 my-4 w-full bg-slate-200 rounded-xl"
               />
             </div>
-            <button className="w-full p-3 text-white bg-blue-600 rounded-xl">
+            <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2  w-full">
               subscribe
             </button>
           </div>
